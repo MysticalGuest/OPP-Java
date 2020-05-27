a79d4dcdd5c433adb02302e19f8c5ec7b8952430
MysticalGuest <1317148109@qq.com>
2020-05-27 17:46:12 +0800

    Create exercise5_1.java

---
+++ CourseExercise/homework/src/com/main/exercise5_1.java
@@ -0,0 +1,39 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class exercise5_1 {
+
+	public static void main(String[] args) {
+		System.out.print("Enter an integer, the input ends if it is 0: ");
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt();
+		
+		int positives = 0 , negatives = 0 , numOfInput = 0;
+		float total = 0 ;
+		
+		while(num!=0) {
+			if(num>0)
+				positives++;
+			else if(num<0)
+				negatives++;
+			
+			numOfInput++;
+			
+			total += num;
+			
+			num = input.nextInt();
+		}
+		
+		System.out.println("The number of positives is " + positives);
+		System.out.println("The number of negatives is " + negatives);
+		System.out.println("The total is " + total);
+		System.out.println("The average is " + total/numOfInput);
+		
+		input.close();
+
+	}
+
+}
