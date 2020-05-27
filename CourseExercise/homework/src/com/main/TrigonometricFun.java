34eef2d7d2bcb41e8ceefc731c87d36033cde2df
MysticalGuest <1317148109@qq.com>
2020-05-27 23:03:37 +0800

    Create TrigonometricFun.java

---
+++ CourseExercise/homework/src/com/main/TrigonometricFun.java
@@ -0,0 +1,28 @@
+package com.main;
+
+// math 4.2.1
+public class TrigonometricFun {
+	public static void main(String[] args) {
+		System.out.println(Math.toDegrees(Math.PI));
+		System.out.println(Math.toDegrees(Math.PI/2));
+		System.out.println(Math.toRadians(180.0));
+		System.out.println(Math.toRadians(30));
+		System.out.println(Math.sin(0));
+		System.out.println(Math.sin(Math.PI));
+		System.out.println(Math.sin(Math.PI/2));
+		System.out.println(Math.sin(Math.PI/3));
+		System.out.println(Math.sin(Math.PI/4));
+		System.out.println(Math.sin(Math.PI/6));
+		System.out.println(Math.sin(Math.toRadians(270)));
+		System.out.println(Math.cos(0));
+		System.out.println(Math.cos(Math.PI));
+		System.out.println(Math.cos(Math.PI/2));
+		System.out.println(Math.cos(Math.PI/3));
+		System.out.println(Math.cos(Math.PI/4));
+		System.out.println(Math.cos(Math.PI/6));
+		System.out.println(Math.asin(0.5));
+		System.out.println(Math.acos(0.5));
+		System.out.println(Math.atan(1.0));
+	}
+
+}
