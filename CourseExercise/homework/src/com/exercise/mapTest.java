5c4a97e2b95cc61ca38ee15c347f6790bdb6ac63
MysticalGuest <1317148109@qq.com>
2020-05-28 16:17:03 +0800

    Create mapTest.java

---
+++ CourseExercise/homework/src/com/exercise/mapTest.java
@@ -0,0 +1,13 @@
+package com.exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class mapTest {
+	public static void main(String[] args) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("a","lisi");
+		map.put("b","");
+		System.out.println(map);
+	}
+}
