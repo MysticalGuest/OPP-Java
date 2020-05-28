76479cdfcf5414d19a2f136cfbb2506a5bb7826f
MysticalGuest <1317148109@qq.com>
2020-05-28 11:45:26 +0800

    Create ReadFileFromURL.java

---
+++ Advanced/src/com/textIO/ReadFileFromURL.java
@@ -0,0 +1,30 @@
+package com.textIO;
+
+import java.util.Scanner;
+
+public class ReadFileFromURL {
+
+	public static void main(String[] args) {
+		System.out.print("Enter a URL: ");
+		String uRLString = new Scanner(System.in).next();
+		
+		try {
+			java.net.URL url = new java.net.URL(uRLString);
+			int count = 0;
+			Scanner input = new Scanner(url.openStream());
+			while (input.hasNext()) {
+				String line = input.nextLine();
+				count += line.length();
+			}
+			
+			System.out.println("The file size is " + count + "characters");
+		} catch (java.net.MalformedURLException ex) {
+			System.out.println("Invalid URL");
+		}
+		catch (java.io.IOException ex) {
+			System.out.println("I/O Errors: no such file");
+		}
+
+	}
+
+}
