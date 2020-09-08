4271b9d4cc09abed7cc5d295cf3ac2af36a33e9a
MysticalGuest <1317148109@qq.com>
2020-09-08 18:26:07 +0800

    Update ThreadA.java

--- MultipleThreads/src/com/threadSafety/ThreadA.java
+++ MultipleThreads/src/com/threadSafety/ThreadA.java
@@ -5,13 +5,28 @@ public class ThreadA extends Thread {
 	private Count count;
 
 	public ThreadA(Count count) {
-		// TODO Auto-generated constructor stub
 		this.count = count;
 	}
 	
 	public void run() {
 		count.add();
 	}
+	
+	public static void main(String[] args) {
 
+		Count count = new Count();
+		for (int i = 0; i < 5; i++) {
+			ThreadA task = new ThreadA(count);
+			task.start();
+		}
+		
+		try {
+			Thread.sleep(100l);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("5个人干完活,最后的值:"+count.num);
+	}
 
 }
