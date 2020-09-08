277a1626e732d5f3279914ee617d93d0bb99c13f
MysticalGuest <1317148109@qq.com>
2020-09-08 18:25:53 +0800

    Create TestSleep2.java

---
+++ MultipleThreads/src/com/newthread/TestSleep2.java
@@ -0,0 +1,43 @@
+package com.newthread;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+// 模拟倒计时
+public class TestSleep2 {
+	public static void main(String[] args) {
+		try {
+			tenDown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// 打印当前系统时间
+		Date startTime = new Date(System.currentTimeMillis());	// 获取当前系统时间
+		
+		System.out.println("Start printing system time...");
+		
+		while (true) {
+			try {
+				Thread.sleep(1000);
+				System.out.println(new SimpleDateFormat("HH:mm:ss").format(startTime));
+				startTime = new Date(System.currentTimeMillis());	// 更新时间
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void tenDown() throws InterruptedException {
+		int num=10;
+		while (true) {
+			Thread.sleep(1000);
+			System.out.println(num--);
+			if (num<=0) {
+				break;
+			}
+		}
+	}
+}
