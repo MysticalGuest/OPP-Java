c441b7729fb8f2c640aa434c20c014844426a15c
MysticalGuest <1317148109@qq.com>
2021-03-18 16:23:49 +0800

    Update test.java

--- SchoolRecruitment/src/com/huawei/test.java
+++ SchoolRecruitment/src/com/huawei/test.java
@@ -4,6 +4,14 @@ public class test {
 
 	public static void main(String[] args) {
 		
+		/**
+		 * 	CharSequence与String都能用于定义字符串，
+		 * 	但CharSequence的值是可读可写序列，而String的值是只读序列
+		 */
+		CharSequence str = "hello world";
+	    CharSequence sub = str.subSequence(6,11);
+	    System.out.println("str.subSequence(6,11): " + sub);
+		
 		StringBuilder stringBuilder = new StringBuilder("abc");
 		
 		System.out.println(stringBuilder);
