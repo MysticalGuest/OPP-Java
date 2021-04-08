30a64d23cb772e65719a373e7ce00cb5b107f4ae
MysticalGuest <1317148109@qq.com>
2021-04-08 11:34:53 +0800

    Update TestDemo.java

--- CourseExercise/TestDemo.java
+++ CourseExercise/TestDemo.java
@@ -2,6 +2,14 @@ package CourseExercise;
 
 public class TestDemo {
   public static void main(String args[]) {
+    
+    int a=1;
+    int b=99;
+
+    int c=a;
+    a=b;
+    b=c;
+
     System.out.println("Run successfully!");
   }
 }
