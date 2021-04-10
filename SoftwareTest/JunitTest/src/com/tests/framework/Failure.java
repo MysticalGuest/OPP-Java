b029f1cbfe9033e8bf8ced53a97800c733a1399d
MysticalGuest <1317148109@qq.com>
2021-04-10 23:28:15 +0800

    Create Failure.java

---
+++ SoftwareTest/JunitTest/src/com/tests/framework/Failure.java
@@ -0,0 +1,13 @@
+package com.tests.framework;
+
+import junit.framework.*;
+
+/**
+ * A test case testing the testing framework.
+ *
+ */
+public class Failure extends TestCase {
+	public void runTest() {
+		fail();
+	}
+}
