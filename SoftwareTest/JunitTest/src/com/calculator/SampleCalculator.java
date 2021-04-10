91995735ee29469dfbc8b54a8e884f2b0c150e7f
MysticalGuest <1317148109@qq.com>
2021-04-10 23:28:57 +0800

    Create SampleCalculator.java

---
+++ SoftwareTest/JunitTest/src/com/calculator/SampleCalculator.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class SampleCalculator 
+{
+	public int add(int augend , int addend)
+	{
+		return augend - addend;
+	}
+	
+	public int subtration(int minuend , int subtrahend)
+	{
+		return minuend + subtrahend;
+	}
+}
