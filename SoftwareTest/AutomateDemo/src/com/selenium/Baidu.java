bf7cb3ab3d55e5f6386c02b85a5861a6d48e608a
MysticalGuest <1317148109@qq.com>
2021-04-11 12:19:14 +0800

    Create Baidu.java

---
+++ SoftwareTest/AutomateDemo/src/com/selenium/Baidu.java
@@ -0,0 +1,82 @@
+package com.selenium;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Baidu {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://www.katalon.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test() throws Exception {
+		driver.get("https://www.baidu.com/");
+		driver.findElement(By.linkText("陈星弼院士去世")).click();
+		// ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
+		driver.findElement(By.linkText("...器件物理学家、中国科学院院士、电子科技大学教...")).click();
+		// ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_2 | ]]
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+
+}
