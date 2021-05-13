0bd0ad38fb8f310f488dad13e21b29a9440730b8
MysticalGuest <1317148109@qq.com>
2021-05-13 12:15:23 +0800

    Create Divide.java

---
+++ SchoolRecruitment/src/com/trendmicro/Divide.java
@@ -0,0 +1,43 @@
+ package com.trendmicro;
+
+import java.util.Scanner;
+ 
+ /**
+ * @author MysteryGuest
+ * @date 2021/05/08
+ * 题目描述：均匀分组
+ * 输入用例：
+ *   第一行：每组人数，逗号隔开
+ *   第二行：分组数
+ * 输出：每组人数
+ */
+public class Divide {
+    
+    public void name() {
+        
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+         * 输入用例：
+         * 1,2,3,3,2
+         * 4
+         * 输出：
+         * 3,3,3,2
+         * */
+        
+        /*
+         * 输入用例：
+         * 1,2,3,3,2,1
+         * 4
+         * 输出：
+         * 3,3,3,3
+         * */
+        
+
+    }
+
+}
