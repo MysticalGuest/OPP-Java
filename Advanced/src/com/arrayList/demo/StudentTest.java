e253fe6458e9a39ed56e15cb98aff1cb42a34097
MysticalGuest <1317148109@qq.com>
2021-05-14 15:20:33 +0800

    Update StudentTest.java

--- Advanced/src/com/arrayList/demo/StudentTest.java
+++ Advanced/src/com/arrayList/demo/StudentTest.java
@@ -3,8 +3,6 @@ package com.arrayList.demo;
 import java.io.*;
 import java.util.ArrayList;
 
-import com.arrayList.entity.Student;
-
 /**
  * @since JDK 1.8
  *
@@ -21,19 +19,19 @@ import com.arrayList.entity.Student;
  */
 public class StudentTest {
     public static void main(String[] args) throws Exception {
-    	String path = "src/com/arrayList/";
+    	String path = "src/";
     	
-        Student s1 = new Student("悔创阿里杰克马",51);
-        Student s2 = new Student("会打一点长几颗",26);
-        Student s3 = new Student("容颜老去蒋青青",32);
-        Student s4 = new Student("将里最丑刘一飞",27);
-        //创建ArrayList集合
+        Student s1 = new Student("Tom", 51);
+        Student s2 = new Student("Jack", 26);
+        Student s3 = new Student("Lisa", 32);
+        Student s4 = new Student("Tara", 27);
+        // 创建ArrayList集合
         ArrayList<Student> list = new ArrayList<Student>();
         list.add(s1);
         list.add(s2);
         list.add(s3);
         list.add(s4);
-        //创建对象操作流
+        // 创建对象操作流
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path+"stu.txt"));
         //将集合写入到文件
         oos.writeObject(list);
@@ -50,5 +48,59 @@ public class StudentTest {
         for (Student student : list1) {
             System.out.println(student);
         }
+    }
+}
+
+class Student implements Serializable{
+    /**
+     * The serializable class Student does not declare a static final serialVersionUID field of type long
+     */
+    private static final long serialVersionUID = 8727101498001680842L;
+    //姓名
+    private String name;
+    //年龄
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /*
+     *  分析:
+     *      创建StringBuilder对象
+     *      先追加一个当前类的类名,以及大括号、成员变量的名字以及='
+     *      再追加成员变量对应的值
+     *      再追加', 成员变量的名字以及=
+     *      最后再次追加成员变量的名字 和 大括号
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Student{name='");
+        sb.append(this.name);
+        sb.append("', age=");
+        sb.append(this.age);
+        sb.append("}");
+        return sb.toString();
     }
 }
