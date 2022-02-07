252eac9e4781168ca771a1df63b13a9705e4af4d
MysticalGuest <1317148109@qq.com>
2022-02-08 00:01:35 +0800

    Create And.java

---
+++ Advanced/src/com/operator/And.java
@@ -0,0 +1,18 @@
+ package com.operator;
+
+ /**
+ * @author MysteryGuest
+ * @date 2022/12/29
+ * java运算符
+ */
+public class And {
+    
+    public static void main(String[] args) {
+        // 一个数和1做与运算是判断奇偶吗？
+        System.out.println(2&1);
+        System.out.println(5&1);
+        System.out.println(10&1);
+        System.out.println(101&1);
+    }
+
+}
