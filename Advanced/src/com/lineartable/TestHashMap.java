c6d088a95bd292d04abb1496a57fd8e101d845b5
MysticalGuest <1317148109@qq.com>
2022-02-08 00:02:09 +0800

    Update TestHashMap.java

--- Advanced/src/com/lineartable/TestHashMap.java
+++ Advanced/src/com/lineartable/TestHashMap.java
@@ -1,6 +1,9 @@
  package com.lineartable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author MysteryGuest
@@ -33,5 +36,19 @@ public class TestHashMap {
         System.out.println(clone==hashMap);
         hashMap.put("A", 1);
         System.out.println("hashmap: "+hashMap+"<>clone"+clone);
+        
+        // getOrDefault()方法
+        System.out.println("测试getOrDefault()方法-------------------------------");
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        for (int i = 0; i < 2; i++) {
+            ArrayList<String> list = map.getOrDefault("Country", new ArrayList<String>());
+            list.add("China"+i);
+            map.put("Country", list);
+        }
+//        System.out.println(map);
+//        ArrayList<String> list = map.getOrDefault("Country", new ArrayList<String>());
+//        list.add("America");
+//        map.put("Country", list);
+        System.out.println(map);
     }
 }
